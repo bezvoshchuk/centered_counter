d7f6292ee7b732bbddd6ea3fd75788674aaf1335
Dmytro Bezvoshchuk <bezvoshchuk@gmail.com>
2024-09-16 14:57:38 +0300

    feat: add count_button logic

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,33 @@
 import { factory } from './factory'
 
-let count = factory(0, 1)
-
-function update_count_and_reset_counter() {}
+let start = 0
+let step = 1
+let count = factory(start, step)
 
 const start_at_control = document.getElementById('start_at') as HTMLInputElement
-
 const step_control = document.getElementById('step') as HTMLInputElement
+const count_button = document.querySelector('.count_button') as HTMLButtonElement
+const current_count = document.querySelector('.current_count') as HTMLSpanElement
 
-start_at_control?.addEventListener('change', () => {})
+// Functions
+function update_count() {
+  current_count.innerHTML = String(count())
+}
 
-step_control?.addEventListener('change', () => {})
+function update_count_and_reset_counter() {
+  count = factory(start, step)
+  update_count()
+}
 
-const count_button = document.querySelector(
-  '.count_button',
-) as HTMLButtonElement
+// Event Listeners
+start_at_control?.addEventListener('change', e => {
+  start = Number((e.target as HTMLInputElement).value || 0)
+  update_count_and_reset_counter()
+})
 
-const current_count = document.querySelector(
-  '.current_count',
-) as HTMLSpanElement
+step_control?.addEventListener('change', (e: Event) => {
+  step = Number((e.target as HTMLInputElement).value || 1)
+  update_count_and_reset_counter()
+})
 
-function update_count() {}
-
-count_button.addEventListener('click', update_count)
+count_button?.addEventListener('click', update_count)
